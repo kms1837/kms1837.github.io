166e177986245ac83b116fcceed2ad462a34d0d5
kms1837 <kms1837@gmail.com>
2020-02-20 15:33:48 +0900

    Updates

---
+++ component---src-pages-page-2-js-431ff25de1392a3ce971.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{p5nM:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),l=a("Wbzz"),c=a("qI5z"),r=a("vrFN");t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-431ff25de1392a3ce971.js.map
---
+++ styles-f11f01186bb3d2f546db.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{BRhB:function(n,w,o){}}]);
+//# sourceMappingURL=styles-f11f01186bb3d2f546db.js.map
